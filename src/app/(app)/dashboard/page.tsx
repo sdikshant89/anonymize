fdf2343c5edd815fd5764f82adaa2c1346797453
sdikshant89 <sdikshant89@gmail.com>
2025-03-17 18:44:20 -0600

    added useeffect to listing dashboard.

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -7,7 +7,7 @@ import { ApiResponse } from '@/types/ApiResponse';
 import { zodResolver } from '@hookform/resolvers/zod';
 import axios, { AxiosError } from 'axios';
 import { useSession } from 'next-auth/react';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 const page = () => {
@@ -47,6 +47,42 @@ const page = () => {
       setIsSwitchLoading(false);
     }
   }, [setValue]);
+
+  const fetchMessages = useCallback(
+    async (refresh: boolean = false) => {
+      setIsLoading(true);
+      setIsSwitchLoading(false);
+      try {
+        const response = await axios.get<ApiResponse>('/api/get-messages');
+        setMessages(response.data.messages || []);
+        if (refresh) {
+          toast({
+            title: 'Refresh Done!',
+            description: 'Showing latest messages',
+          });
+        }
+      } catch (error) {
+        const axiosError = error as AxiosError<ApiResponse>;
+        toast({
+          title: 'Error',
+          description:
+            axiosError.response?.data.message ||
+            'Failed to fetch error message',
+          variant: 'destructive',
+        });
+      } finally {
+        setIsLoading(false);
+        setIsSwitchLoading(false);
+      }
+    },
+    [setIsLoading, setMessages]
+  );
+
+  useEffect(() => {
+    if (!session || !session.user) return;
+    fetchMessages();
+    fetchAcceptMessages();
+  }, [session, setValue, fetchAcceptMessages, fetchMessages]);
 
   return <div>Dashboard</div>;
 };
